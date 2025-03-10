754694a8a5ac50056a39bd076f79fae514cf5584
berke.yz <berkeyilmaz189@gmail.com>
2025-03-10 10:36:10 +0300

    main calorie left ui done

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -41,6 +41,7 @@ dependencies {
     implementation(libs.constraintlayout)
     implementation(libs.navigation.fragment)
     implementation(libs.navigation.ui)
+    implementation (libs.material.v190)
     testImplementation(libs.junit)
     androidTestImplementation(libs.ext.junit)
     androidTestImplementation(libs.espresso.core)
