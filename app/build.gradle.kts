6e92c480638b715579220c33c0c91ca82023f1ae
berke.yz <berkeyilmaz189@gmail.com>
2025-03-19 14:25:29 +0300

    first version of home view's ui done

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -42,6 +42,7 @@ dependencies {
     implementation(libs.navigation.fragment)
     implementation(libs.navigation.ui)
     implementation (libs.material.v190)
+    implementation(libs.activity)
     testImplementation(libs.junit)
     androidTestImplementation(libs.ext.junit)
     androidTestImplementation(libs.espresso.core)
