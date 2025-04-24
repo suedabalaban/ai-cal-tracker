3dde2e3704fe393b9605002909cde6a19b86a5a4
berke.yz <berkeyilmaz189@gmail.com>
2025-04-24 21:42:17 +0300

    navigation deleted from CalorieTracker

--- app/src/main/java/com/duzceders/aicaltracker/features/calorie_tracker/CalorieTracker.java
+++ app/src/main/java/com/duzceders/aicaltracker/features/calorie_tracker/CalorieTracker.java
@@ -1,4 +1,4 @@
-package com.duzceders.aicaltracker.home;
+package com.duzceders.aicaltracker.features.calorie_tracker;
 
 import android.Manifest;
 import android.content.Intent;
@@ -9,7 +9,6 @@ import android.os.Build;
 import android.os.Bundle;
 import android.provider.MediaStore;
 import android.util.Log;
-import android.view.MenuItem;
 import android.view.View;
 import android.widget.TextView;
 import android.widget.Toast;
@@ -18,27 +17,21 @@ import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.appcompat.app.ActionBarDrawerToggle;
 import androidx.appcompat.app.AppCompatActivity;
-import androidx.appcompat.widget.Toolbar;
 import androidx.core.app.ActivityCompat;
 import androidx.core.content.ContextCompat;
-import androidx.core.view.GravityCompat;
 import androidx.drawerlayout.widget.DrawerLayout;
-import androidx.appcompat.app.AppCompatActivity;
-import androidx.core.app.ActivityCompat;
-import androidx.core.content.ContextCompat;
 import androidx.lifecycle.ViewModelProvider;
 
 import com.duzceders.aicaltracker.R;
 import com.duzceders.aicaltracker.product.models.User;
 import com.duzceders.aicaltracker.product.service.FirebaseRepository;
-import com.duzceders.aicaltracker.profile.ProfileActivity;
 import com.duzceders.aicaltracker.product.service.manager.CloudinaryServiceManager;
 import com.google.android.material.floatingactionbutton.FloatingActionButton;
 import com.google.android.material.navigation.NavigationView;
 import com.google.android.material.progressindicator.CircularProgressIndicator;
 import com.google.firebase.FirebaseApp;
 
-public class CalorieTracker extends AppCompatActivity implements NavigationView.OnNavigationItemSelectedListener {
+public class CalorieTracker extends AppCompatActivity {
 
     private static final int CAMERA_PERMISSION_REQUEST_CODE = 100;
     private static final int CAMERA_INTENT_REQUEST_CODE = 101;
@@ -70,9 +63,8 @@ public class CalorieTracker extends AppCompatActivity implements NavigationView.
         super.onCreate(savedInstanceState);
         FirebaseApp.initializeApp(this);
         firebaseRepository = new FirebaseRepository();
-        setContentView(R.layout.activity_calorie_tracker);
+        setContentView(R.layout.fragment_calorie_tracker);
 
-        setupDrawer();
 
         CalorieTrackerViewModel viewModel = new ViewModelProvider(this).get(CalorieTrackerViewModel.class);
 
@@ -90,65 +82,6 @@ public class CalorieTracker extends AppCompatActivity implements NavigationView.
         setClickListeners();
     }
 
-    private void setupDrawer() {
-        // Initialize the drawer layout
-        drawerLayout = findViewById(R.id.drawerLayout);
-        navigationView = findViewById(R.id.navigationView);
-        Toolbar toolbar = findViewById(R.id.toolbar);
-
-        // Setup toolbar
-        setSupportActionBar(toolbar);
-
-        // Remove default title
-        if (getSupportActionBar() != null) {
-            getSupportActionBar().setDisplayShowTitleEnabled(false);
-        }
-
-        // Setup drawer toggle
-        toggle = new ActionBarDrawerToggle(
-                this, drawerLayout, toolbar,
-                R.string.navigation_drawer_open,
-                R.string.navigation_drawer_close);
-
-        drawerLayout.addDrawerListener(toggle);
-        toggle.syncState();
-
-        // Set navigation item selected listener
-        navigationView.setNavigationItemSelectedListener(this);
-    }
-
-    @Override
-    public boolean onNavigationItemSelected(@NonNull MenuItem item) {
-        int id = item.getItemId();
-        Class<?> currentActivity = CalorieTracker.this.getClass();
-
-        if (id == R.id.nav_home && currentActivity != CalorieTracker.class) {
-            Toast.makeText(this, "Ana sayfaya geçiş yapılıyor...", Toast.LENGTH_SHORT).show();
-            Intent intent = new Intent(this, CalorieTracker.class);
-            startActivity(intent);
-        } else if (id == R.id.nav_profile) {
-            Intent intent = new Intent(this, ProfileActivity.class);
-            startActivity(intent);
-        } else if (id == R.id.nav_settings ) {
-            Toast.makeText(this, "Ayarlar seçildi", Toast.LENGTH_SHORT).show();
-
-        } else if (id == R.id.nav_logout) {
-            Toast.makeText(this, "Çıkış yapılıyor...", Toast.LENGTH_SHORT).show();
-
-        }
-
-        drawerLayout.closeDrawer(GravityCompat.START);
-        return true;
-    }
-
-    @Override
-    public void onBackPressed() {
-        if (drawerLayout.isDrawerOpen(GravityCompat.START)) {
-            drawerLayout.closeDrawer(GravityCompat.START);
-        } else {
-            super.onBackPressed();
-        }
-    }
 
     private void updateUiWithUserData(User user) {
         String welcomeMessage = getResources().getString(R.string.welcome, user.getName() + " " + user.getSurname());
