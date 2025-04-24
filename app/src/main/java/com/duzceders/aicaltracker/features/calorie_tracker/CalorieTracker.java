fdec8295a638fc91de136077ccb74554ec016b6d
berke.yz <berkeyilmaz189@gmail.com>
2025-04-24 21:45:53 +0300

    binding added for calorie tracker

--- app/src/main/java/com/duzceders/aicaltracker/features/calorie_tracker/CalorieTracker.java
+++ app/src/main/java/com/duzceders/aicaltracker/features/calorie_tracker/CalorieTracker.java
@@ -1,6 +1,7 @@
 package com.duzceders.aicaltracker.features.calorie_tracker;
 
 import android.Manifest;
+import android.annotation.SuppressLint;
 import android.content.Intent;
 import android.content.pm.PackageManager;
 import android.graphics.Bitmap;
@@ -10,51 +11,36 @@ import android.os.Bundle;
 import android.provider.MediaStore;
 import android.util.Log;
 import android.view.View;
-import android.widget.TextView;
 import android.widget.Toast;
 
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
-import androidx.appcompat.app.ActionBarDrawerToggle;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.core.app.ActivityCompat;
 import androidx.core.content.ContextCompat;
-import androidx.drawerlayout.widget.DrawerLayout;
 import androidx.lifecycle.ViewModelProvider;
 
 import com.duzceders.aicaltracker.R;
+import com.duzceders.aicaltracker.databinding.ActivityCalorieTrackerBinding;
 import com.duzceders.aicaltracker.product.models.User;
 import com.duzceders.aicaltracker.product.service.FirebaseRepository;
 import com.duzceders.aicaltracker.product.service.manager.CloudinaryServiceManager;
 import com.google.android.material.floatingactionbutton.FloatingActionButton;
-import com.google.android.material.navigation.NavigationView;
-import com.google.android.material.progressindicator.CircularProgressIndicator;
 import com.google.firebase.FirebaseApp;
 
+import java.util.Objects;
+
 public class CalorieTracker extends AppCompatActivity {
+
+    private ActivityCalorieTrackerBinding binding;
 
     private static final int CAMERA_PERMISSION_REQUEST_CODE = 100;
     private static final int CAMERA_INTENT_REQUEST_CODE = 101;
     private static final int GALLERY_PERMISSION_REQUEST_CODE = 102;
     private static final int GALLERY_INTENT_REQUEST_CODE = 103;
 
-    private FirebaseRepository firebaseRepository;
-
-    private DrawerLayout drawerLayout;
-    private NavigationView navigationView;
-    private ActionBarDrawerToggle toggle;
     private static final String TAG = "CalorieTracker";
 
-    private TextView totalCaloriesValue;
-    private TextView totalProteinValue;
-    private TextView totalCarbsValue;
-    private TextView totalFatsValue;
-    private TextView welcomeText;
-
-    private CircularProgressIndicator circularProgressCalories;
-    private CircularProgressIndicator circularProgressProtein;
-    private CircularProgressIndicator circularProgressCarbs;
-    private CircularProgressIndicator circularProgressFats;
 
     private CloudinaryServiceManager cloudinaryServiceManager;
 
@@ -62,14 +48,14 @@ public class CalorieTracker extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         FirebaseApp.initializeApp(this);
-        firebaseRepository = new FirebaseRepository();
-        setContentView(R.layout.fragment_calorie_tracker);
+        FirebaseRepository firebaseRepository = new FirebaseRepository();
+        binding = ActivityCalorieTrackerBinding.inflate(getLayoutInflater());
+        setContentView(R.layout.activity_calorie_tracker);
 
 
         CalorieTrackerViewModel viewModel = new ViewModelProvider(this).get(CalorieTrackerViewModel.class);
 
         cloudinaryServiceManager = new CloudinaryServiceManager(this);
-        initializeViews();
 
         viewModel.getUserData().observe(this, user -> {
             if (user != null) {
@@ -85,41 +71,29 @@ public class CalorieTracker extends AppCompatActivity {
 
     private void updateUiWithUserData(User user) {
         String welcomeMessage = getResources().getString(R.string.welcome, user.getName() + " " + user.getSurname());
-        welcomeText.setText(welcomeMessage);
+        binding.welcomeText.setText(welcomeMessage);
 
-        totalCaloriesValue.setText(String.valueOf(user.getDaily_calorie_needs_left()));
-        totalProteinValue.setText(String.valueOf(user.getDaily_macros().getDaily_proteins_left_g()));
-        totalCarbsValue.setText(String.valueOf(user.getDaily_macros().getDaily_carbs_left_g()));
-        totalFatsValue.setText(String.valueOf(user.getDaily_macros().getDaily_fats_left_g()));
+        binding.totalCaloriesValue.setText(String.valueOf(user.getDaily_calorie_needs_left()));
+        binding.totalProteinValue.setText(String.valueOf(user.getDaily_macros().getDaily_proteins_left_g()));
+        binding.totalCarbsValue.setText(String.valueOf(user.getDaily_macros().getDaily_carbs_left_g()));
+        binding.totalFatsValue.setText(String.valueOf(user.getDaily_macros().getDaily_fats_left_g()));
 
         int progressCalories = calculateProgress(user.getDaily_calorie_needs_left(), user.getDaily_calorie_needs());
         int progressProtein = calculateProgress(user.getDaily_macros().getDaily_proteins_left_g(), user.getDaily_macros().getDaily_proteins_need_g());
         int progressCarbs = calculateProgress(user.getDaily_macros().getDaily_carbs_left_g(), user.getDaily_macros().getDaily_carbs_need_g());
         int progressFats = calculateProgress(user.getDaily_macros().getDaily_fats_left_g(), user.getDaily_macros().getDaily_fats_need_g());
 
-        circularProgressCalories.setProgress(progressCalories);
-        circularProgressProtein.setProgress(progressProtein);
-        circularProgressCarbs.setProgress(progressCarbs);
-        circularProgressFats.setProgress(progressFats);
-    }
-
-    private void initializeViews() {
-        totalCaloriesValue = findViewById(R.id.totalCaloriesValue);
-        totalProteinValue = findViewById(R.id.totalProteinValue);
-        totalCarbsValue = findViewById(R.id.totalCarbsValue);
-        totalFatsValue = findViewById(R.id.totalFatsValue);
-        welcomeText = findViewById(R.id.welcomeText);
-
-        circularProgressCalories = findViewById(R.id.circularProgressCalories);
-        circularProgressProtein = findViewById(R.id.circularProgressProtein);
-        circularProgressCarbs = findViewById(R.id.circularProgressCarbs);
-        circularProgressFats = findViewById(R.id.circularProgressFats);
+        binding.circularProgressCalories.setProgress(progressCalories);
+        binding.circularProgressProtein.setProgress(progressProtein);
+        binding.circularProgressCarbs.setProgress(progressCarbs);
+        binding.circularProgressFats.setProgress(progressFats);
     }
 
     private int calculateProgress(double left, double target) {
         return (int) (((float) (target - left) / target) * 100);
     }
 
+    @SuppressLint("QueryPermissionsNeeded")
     private void openCamera() {
         Intent cameraIntent = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
         if (cameraIntent.resolveActivity(getPackageManager()) != null) {
@@ -127,6 +101,7 @@ public class CalorieTracker extends AppCompatActivity {
         }
     }
 
+    @SuppressLint("IntentReset")
     private void openGallery() {
         Intent intent = new Intent(Intent.ACTION_PICK, MediaStore.Images.Media.EXTERNAL_CONTENT_URI);
         intent.setType("image/*");
@@ -176,9 +151,8 @@ public class CalorieTracker extends AppCompatActivity {
 
         if (resultCode != RESULT_OK) return;
         if (requestCode == CAMERA_INTENT_REQUEST_CODE && data != null) {
-            Bitmap photo = (Bitmap) data.getExtras().get("data");
+            Bitmap photo = (Bitmap) Objects.requireNonNull(data.getExtras()).get("data");
             if (photo == null) return;
-            Log.d(TAG, "Fotoğraf çekildi!");
             showToast(getString(R.string.image_uploading));
             uploadImageToCloudinaryFromCamera(photo);
         }
@@ -186,7 +160,6 @@ public class CalorieTracker extends AppCompatActivity {
         if (requestCode == GALLERY_INTENT_REQUEST_CODE && data != null) {
             Uri selectedImageUri = data.getData();
             if (selectedImageUri == null) return;
-            Log.d(TAG, "Fotoğraf seçildi: " + selectedImageUri);
 
             showToast(getString(R.string.image_uploading));
             uploadImageToCloudinaryFromGallery(selectedImageUri);
